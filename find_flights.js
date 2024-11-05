cd269bab80437abd6c5afdb9e2a535e7e7fcddf9
ggmu2016 <ggmu2016@utexas.edu>
2024-11-04 21:56:14 -0600

    fixed some stuff, need to fix date stuff in find_flights.js

--- find_flights.js
+++ find_flights.js
@@ -5,12 +5,13 @@ function findFlights(orig, dst, dep, arr, seats) {
         .then(response => response.text())
         .then(data => {
             const parser = new DOMParser();
-            const xmlDoc = parser.parseFromString(data, 'application/xml');
+            const xmlDoc = parser.parseFromString(data, 'text/xml');
             const records = xmlDoc.getElementsByTagName('record');
 
             // upper and lower bounds of input date
+
             const upperbound = new Date();
-            upperbound.setDate(dep.getDate()+3)
+            upperbound.setDate(dep.getDate()+3);
             const lowerbound = new Date();
             lowerbound.setDate(dep.getDate()-3)
 
@@ -21,14 +22,14 @@ function findFlights(orig, dst, dep, arr, seats) {
 
             for (let i = 0; i < records.length; i++) {
                 const id = records[i].getElementsByTagName("id")[0].textContent;
-                const origin = records[i].getElementsByTagName("origin")[1].textContent;
-                const dest = records[i].getElementsByTagName("dest")[2].textContent;
-                const dep_date = records[i].getElementsByTagName("dep_date")[3].textContent;
-                const arr_date = records[i].getElementsByTagName("arr_date")[4].textContent;
-                const dep_time = records[i].getElementsByTagName("dep_time")[5].textContent;
-                const arr_time = records[i].getElementsByTagName("arr_time")[6].textContent;
-                const num_seats = parseInt(records[i].getElementsByTagName("num_seats")[7].textContent);
-                const price = parseInt(records[i].getElementsByTagName("price")[8].textContent);
+                const origin = records[i].getElementsByTagName("origin")[0].textContent;
+                const dest = records[i].getElementsByTagName("dest")[0].textContent;
+                const dep_date = records[i].getElementsByTagName("dep_date")[0].textContent;
+                const arr_date = records[i].getElementsByTagName("arr_date")[0].textContent;
+                const dep_time = records[i].getElementsByTagName("dep_time")[0].textContent;
+                const arr_time = records[i].getElementsByTagName("arr_time")[0].textContent;
+                const num_seats = parseInt(records[i].getElementsByTagName("num_seats")[0].textContent);
+                const price = parseInt(records[i].getElementsByTagName("price")[0].textContent);
 
                 // converting text dates in xml to js date types
                 let [dep_month, dep_day, dep_year] = dep_date.split("/").map(Number);
@@ -38,12 +39,28 @@ function findFlights(orig, dst, dep, arr, seats) {
                     let adate = new Date(arr_year,arr_month-1, arr_day);
                 }
 
+                console.log("orig", orig);
+                console.log("origin", origin.toLowerCase());
+                console.log("dest", dest.toLowerCase());
+                console.log("dst", dst);
+                console.log("num_seats",num_seats);
+                console.log("seats",seats);
+                console.log("ddate",ddate);
+                console.log("dep",dep)
+                if (ddate===dep){
+                    console.log("fuck yea mf")
+                }
+                console.log("lowerbound",lowerbound);
+                console.log("upperbound",upperbound);
+
+
                 // checks if everything meets the user input and populates the arrays accordingly
-                if (seats>=num_seats && orig===origin && dest===dest && ddate.getDate()>=lowerbound
+                if (seats<=num_seats && orig===origin.toLowerCase() && dest.toLowerCase()===dst && ddate.getDate()>=lowerbound
                     && ddate.getDate() <=upperbound){
+                    console.log("hello")
                     curr_flight = [id,origin,dest,dep_date,dep_time,arr_date,arr_time,num_seats,price];
                     allFlights.push(curr_flight);
-                    if (dep.getDate()===ddate.getDate()){
+                    if (dep===ddate){
                         flightsWithDate.push(curr_flight);
                     }
                 }
