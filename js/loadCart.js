9bfb03aac76b8900abff8174a1563cb67746f8fc
Johan <Johan@johan.com>
2024-11-17 17:43:40 -0600

    Joey - Fixed some issues on the cart page

--- js/loadCart.js
+++ js/loadCart.js
@@ -1,3 +1,6 @@
+const jsonUpdateURL = "http://localhost:3000/update-json";
+const jsonFetchURL = "../json/data.json";
+
 async function fetchData(url) {
     const response = await fetch(url);
     return response.json();
@@ -10,68 +13,59 @@ async function fetchXML(url) {
     return parser.parseFromString(text, "application/xml");
 }
 
-async function updateJsonData(ssn, updatedCart, jsonURL) {
-    const customerData = await fetchData(jsonURL);
-    customerData.passengers["passenger1"].cart = updatedCart;
-
-    await fetch(jsonURL, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(customerData)
-    });
-}
-
-
-async function updateJsonBooking(passenger_id, booking_object, res_id, jsonURL, flightsXML) {
-    const customerData = await fetchData(jsonURL); // Fetch customer data from local JSON
-    let booked_length = booking_object.res_id = customerData.passengers["passenger1"].booked.length // Get the length of the booked array to increment the last reservation ID
-    booking_object.res_id = customerData.passengers["passenger1"].booked[booked_length - 1].res_id + 1; // Current reservation ID is last reservation ID plus one
-    customerData.passengers["passenger1"].booked.push(booking_object); // Add the new booking
-
-    if(!booking_object.return_f){
-        let oneways = customerData.passengers["passenger1"].cart.oway;
-        console.log(oneways);
-        console.log(booking_object.departure);
-        let index = oneways.findIndex(item => item.toString() === booking_object.departure.toString());
-        // Check if the element was found
-        if (index !== -1) { // Remove the element using splice
-            customerData.passengers["passenger1"].cart.oway = oneways.splice(index, 1);
-        }
-        console.log("1");
-    } else{
-        let rtrips_d = customerData.passengers["passenger1"].cart.rtrip.departures;
-        let rtrips_r = customerData.passengers["passenger1"].cart.rtrip.returns;
-
-        let index = rtrips_d.findIndex(item => item.name === booking_object.departure);
-        // Check if the element was found
-        if (index !== -1) { // Remove the element using splice
-            customerData.passengers["passenger1"].cart.rtrip.departures = rtrips_d.splice(index, 1);
-        }
-
-        index = rtrips_r.findIndex(item => item.name === booking_object.return_f);
-        // Check if the element was found
-        if (index !== -1) { // Remove the element using splice
-            customerData.passengers["passenger1"].cart.rtrip.returns = rtrips_r.splice(index, 1);
-        }
-        console.log("2");
-    }
-
-    let cdata_string = JSON.stringify(customerData);
-    console.log(typeof cdata_string);
-
+async function updateJsonData(updatedData) {
     await fetch("http://localhost:3000/update-json", { // Update the JSON file
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
-        body: cdata_string
+        body: JSON.stringify(updatedData)
     });
-    displayBooked(customerData.passengers["passenger1"].booked, flightsXML);
 }
 
-function displayCart(cart, flightsXML, ssn, jsonURL) {
+async function deleteFromCart(customerData, booking_object) {
+    if (!booking_object.return_f) {
+        let oneways = customerData.passengers["passenger1"].cart.oway;
+        let index = oneways.findIndex(item => item.valueOf() === booking_object.departure.valueOf());
+
+        // Check if the element was found
+        if (index !== -1) { // Remove the element using splice
+            oneways.splice(index, 1);
+        }
+    } else {
+        let rtrips_d = customerData.passengers["passenger1"].cart.rtrip.departures;
+        let rtrips_r = customerData.passengers["passenger1"].cart.rtrip.returns;
+
+        let index = rtrips_d.findIndex(item => item.valueOf() === booking_object.departure.valueOf());
+        // Check if the element was found
+        if (index !== -1) { // Remove the element using splice
+            rtrips_d.splice(index, 1);
+        }
+
+        index = rtrips_r.findIndex(item => item.valueOf() === booking_object.return_f.valueOf());
+        // Check if the element was found
+        if (index !== -1) { // Remove the element using splice
+            rtrips_r.splice(index, 1);
+        }
+    }
+}
+
+async function updateJsonBooking(passenger_id, booking_object, res_id, flightsXML) {
+    var customerdata = await fetchData(jsonFetchURL); // Fetch customer data from local JSON
+    let booked_length = booking_object.res_id = customerdata.passengers["passenger1"].booked.length // Get the length of the booked array to increment the last reservation ID
+    booking_object.res_id = customerdata.passengers["passenger1"].booked[booked_length - 1].res_id + 1; // Current reservation ID is last reservation ID plus one
+    deleteFromCart(customerdata, booking_object);
+    customerdata.passengers["passenger1"].booked.push(booking_object); // Add the new booking
+    updateJsonData(customerdata);
+    displayCart(getCart(customerdata), flightsXML);
+    displayBooked(customerdata.passengers["passenger1"].booked, flightsXML);
+    location.reload(true);
+}
+
+function displayCart(cart, flightsXML) {
     const cartDiv = document.getElementById('cart');
     cartDiv.innerHTML = ''; // Clear previous content
 
     cart.forEach(flightId => {
+        console.log(flightId);
         const flight_mode = flightId[0];
         var depart_flight_id = "";
         var return_flight_id = "";
@@ -106,41 +100,49 @@ function displayCart(cart, flightsXML, ssn, jsonURL) {
             }
             flightContainer.innerHTML +=
                 ` <h3>Flight ID: ${flightId}</h3> 
+                    <p hidden="true" id="passCount-${flightId}">1</p>
                     <p><strong>Origin:</strong> ${origin}</p> 
                     <p><strong>Destination:</strong> ${destination}</p> 
                     <p><strong>Departure:</strong> ${departureDate} at ${departureTime}</p> 
                     <p><strong>Arrival:</strong> ${arrivalDate} at ${arrivalTime}</p> 
                     <p><strong>Number of Seats:</strong> ${numSeats}</p> 
                     <p><strong>Price:</strong> Adult: $${price} Child: $${price_child} Infant: $${price_infant}</p> 
-                    <form id="passenger-form">
+                    <form id="passenger-form-${flightId}">
                         <div class="passenger-form" id="passenger-1">
                             <h2>Passenger 1</h2>
                             <div class="form-field">
-                                <label for="first-name-1">First Name</label>
+                                <label for="first-name-${flightId}-1">First Name</label>
                                 <input type="text" id="first-name-1" name="first-name-1" required>
                             </div>
                             <div class="form-field">
-                                <label for="last-name-1">Last Name</label>
+                                <label for="last-name-${flightId}-1">Last Name</label>
                                 <input type="text" id="last-name-1" name="last-name-1" required>
                             </div>
                             <div class="form-field">
-                                <label for="dob-1">Date of Birth</label>
-                                <input type="date" id="dob-1" name="dob-1" required>
+                                <label for="dob-${flightId}-1">Date of Birth</label>
+                                <input type="date" id="dob-${flightId}-1" name="dob-1" required>
                             </div>
                             <div class="form-field">
-                                <label for="ssn-1">SSN</label>
-                                <input type="text" id="ssn-1" name="ssn-1" required>
+                                <label for="ssn-${flightId}-1">SSN</label>
+                                <input type="text" id="ssn-${flightId}-1" name="ssn-1" required>
                             </div>
                         </div>
                     </form>
-                    <button class="add-passenger" onclick="addPassenger()">+ Add Passenger</button><br>
+                    <button class="add-passenger" onclick="addPassenger(${flightId})">+ Add Passenger</button><br>
                     <br>
                     <button class="delete-button">Delete</button><br><br>
                     <button id=\"bookButton\" class="book-button">Book</button> `;
             flightContainer.querySelector('.delete-button').addEventListener('click', async () => { // Remove flight from cart
-                const updatedCart = cart.filter(id => id !== orig_flightId);
-                await updateJsonData(ssn, updatedCart, jsonURL); // Reload cart
-                displayCart(updatedCart, flightsXML, ssn, jsonURL);
+                var newBooking = {
+                    "res_id": "",
+                    "departure": depart_flight_id,
+                    "return_f": return_flight_id,
+                    "passengers": []
+                }
+                let customerdata = await fetchData(jsonFetchURL);
+                deleteFromCart(customerdata, newBooking)
+                updateJsonData(customerdata); // Reload cart
+                displayCart(getCart(customerdata), flightsXML);
             });
             cartDiv.appendChild(flightContainer);
             document.getElementById("bookButton").addEventListener('click', async () => { // Remove flight from cart
@@ -151,6 +153,7 @@ function displayCart(cart, flightsXML, ssn, jsonURL) {
                     "passengers": []
                 }
 
+                let passengerCount = document.getElementById("passCount"+flightId).textContent.valueOf();
                 var passengers = [];
                 for (let i = 1; i <= passengerCount; i++) {
                     passengers.push({
@@ -163,9 +166,8 @@ function displayCart(cart, flightsXML, ssn, jsonURL) {
                     record.querySelector('num_seats').textContent = numSeats; // update num seats in the XML file
                 }
                 newBooking.passengers = passengers;
-                await updateJsonBooking("passenger1", newBooking, 1, jsonURL, flightsXML); // Reload cart
-                const updatedCart = cart.filter(id => id !== orig_flightId);
-                displayCart(updatedCart, flightsXML, ssn, jsonURL);
+                updateJsonBooking("passenger1", newBooking, 1, flightsXML); // Reload cart
+                displayCart(getCart(await fetchData(jsonFetchURL)), flightsXML);
             });
         }
     });
@@ -229,68 +231,82 @@ function displayBooked(bookedFlights, flightsXML) {
     });
 }
 
-
-let passengerCount = 1;
-
-function addPassenger() {
+function addPassenger(flightId) {
+    let passengerCount = document.getElementById("passCount-"+flightId).textContent.valueOf();
     passengerCount++;
-    const form = document.getElementById('passenger-form');
+    document.getElementById("passCount-"+flightId).textContent = passengerCount;
+    const form = document.getElementById(`passenger-form-${flightId}`);
     const newPassengerDiv = document.createElement('div');
     newPassengerDiv.className = 'passenger-form';
     newPassengerDiv.id = 'passenger-' + passengerCount;
-
     newPassengerDiv.innerHTML = `
                 <h2>Passenger ${passengerCount}</h2>
                 <div class="form-field">
-                    <label for="first-name-${passengerCount}">First Name</label>
-                    <input type="text" id="first-name-${passengerCount}" name="first-name-${passengerCount}" required>
+                    <label for="first-name-${flightId}-${passengerCount}">First Name</label>
+                    <input type="text" id="first-name-${flightId}-${passengerCount}" name="first-name-${passengerCount}" required>
                 </div>
                 <div class="form-field">
-                    <label for="last-name-${passengerCount}">Last Name</label>
+                    <label for="last-name-${flightId}-${passengerCount}">Last Name</label>
                     <input type="text" id="last-name-${passengerCount}" name="last-name-${passengerCount}" required>
                 </div>
                 <div class="form-field">
-                    <label for="dob-${passengerCount}">Date of Birth</label>
-                    <input type="date" id="dob-${passengerCount}" name="dob-${passengerCount}" required>
+                    <label for="dob-${flightId}-${passengerCount}">Date of Birth</label>
+                    <input type="date" id="dob-${flightId}-${passengerCount}" name="dob-${passengerCount}" required>
                 </div>
                 <div class="form-field">
-                    <label for="ssn-${passengerCount}">SSN</label>
-                    <input type="text" id="ssn-${passengerCount}" name="ssn-${passengerCount}" required>
+                    <label for="ssn-${flightId}-${passengerCount}">SSN</label>
+                    <input type="text" id="ssn-${flightId}-${passengerCount}" name="ssn-${passengerCount}" required>
                 </div>
             `;
 
     form.appendChild(newPassengerDiv);
 }
 
-async function loadCart() {
-    const jsonURL = '../json/data.json';
-    const xmlURL = '../xml/data.xml';
-    const customerData = await fetchData(jsonURL);
-    const currentCustomer = "" // TODO: Needs to be updated
-
-    const flightsXML = await fetchXML(xmlURL);
-
+function getCart(customerData) {
+    var concat_cart = [];
     const cart_oneway = customerData.passengers["passenger1"].cart.oway;
-    cart_oneway.forEach(((cart, index) => {
-        cart_oneway[index] = "1" + cart;
-    }));
+    try {
+        cart_oneway.forEach(((cart, index) => {
+            cart_oneway[index] = "1" + cart;
+        }));
+        concat_cart = concat_cart.concat(cart_oneway);
+    } catch (error) {
+    }
 
-    const cart_rtrip_d = customerData.passengers["passenger1"].cart.rtrip.departures;
-    cart_rtrip_d.forEach(((cart, index) => {
-        cart_rtrip_d[index] = "2" + cart;
-    }));
+    try {
+        const cart_rtrip_d = customerData.passengers["passenger1"].cart.rtrip.departures;
+        cart_rtrip_d.forEach(((cart, index) => {
+            cart_rtrip_d[index] = "2" + cart;
+        }));
+        concat_cart = concat_cart.concat(cart_rtrip_d);
+    } catch (error) {
+    }
 
-    const cart_rtrip_r = customerData.passengers["passenger1"].cart.rtrip.returns;
-    cart_rtrip_r.forEach(((cart, index) => {
-        cart_rtrip_r[index] = "3" + cart;
-    }));
+    try {
+        const cart_rtrip_r = customerData.passengers["passenger1"].cart.rtrip.returns;
+        cart_rtrip_r.forEach(((cart, index) => {
+            cart_rtrip_r[index] = "3" + cart;
+        }));
+        concat_cart = concat_cart.concat(cart_rtrip_r);
+    } catch (error) {
+    }
 
-    const concat_cart = [...cart_oneway, ...cart_rtrip_d, ...cart_rtrip_r];
-    const customerssn = customerData.passengers["passenger1"].ssn;
-    displayCart(concat_cart, flightsXML, customerssn, jsonURL);
+    return concat_cart;
+}
+
+async function loadCart() {
+    const customerData = await fetchData(jsonFetchURL);
+    const flightsXML = await fetchXML("../xml/data.xml");
+
+    var concat_cart = getCart(customerData);
+    if (concat_cart.length != 0) {
+        displayCart(concat_cart, flightsXML);
+    }
 
     let customer_booked = customerData.passengers["passenger1"].booked;
-    displayBooked(customer_booked, flightsXML);
+    if (customer_booked.length != 0) {
+        displayBooked(customer_booked, flightsXML);
+    }
 }
 
 loadCart();
